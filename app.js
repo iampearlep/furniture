116f4289b83d43cb52efda03280cbae4495cc437
iampearlep <damilolaprincess@gmail.com>
2022-08-31 00:59:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let w = document.documentElement.clientWidth || window.innerWidth;
+
+ if (w <= 680) {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+ } else {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+ }
